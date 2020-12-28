6e9c8d9e74a56c8f58f6d9d071ba8ad73b7ca0ba
Marie <marieborisova@gmail.com>
2020-12-28 12:23:03 +0300

    OK

---
+++ app/src/main/java/com/example/fragments3_6_1/CheckBoxFragment.java
@@ -0,0 +1,18 @@
+package com.example.fragments3_6_1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class CheckBoxFragment extends Fragment {
+
+    @Override
+    // Переопределяем метод onCreateView
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.checkbox_fragment, container, false);
+        return view;
+    }
+}
